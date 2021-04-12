11e0c27349f6f6184ffaeb8f354d147f9562ae47
nimowairimu <nimowairimu25@gmail.com>
2021-04-12 09:58:40 +0300

    Create a user registration form using django-registration .

---
+++ myhood/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include,path,re_path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    # path('',views.index,name = 'home'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+]
