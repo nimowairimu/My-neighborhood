106e1874f526d5c9658db9f3197fc98af5cb5704
nimowairimu <nimowairimu25@gmail.com>
2021-04-12 12:38:54 +0300

    Update sidebar.

--- myhood/urls.py
+++ myhood/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('register/',views.register, name='registration'),
     path('all-hoods/', views.hoods, name='hood'),
-    path('profile/', views.profile, name='profile'),
+    path('profile/<username>', views.profile, name='profile'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
     path('new-hood/', views.create_hood, name='new-hood'),
